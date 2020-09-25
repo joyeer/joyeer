c57b8b25b73d547288686be29deae99f9492030c
Qing Xu <xuqing@decompile.io>
2020-09-25 09:15:18 +0800

    Add some files

---
+++ runtime/sys/set.h
@@ -0,0 +1,11 @@
+#ifndef __joyeer_runtime_sys_set_h__
+#define __joyeer_runtime_sys_set_h__
+
+#include "runtime/runtime.h"
+#include "runtime/object.h"
+
+struct JrSet: public JrObject {
+    
+};
+
+#endif
