ad1117408601edee1dfc28505f7bbb4bbfb39387
Qing Xu <xuqing@decompile.io>
2021-04-19 23:03:12 +0800

    declaration all primary type's descriptors, and unittests for these types

--- include/joyeer/common/descriptor.h
+++ include/joyeer/common/descriptor.h
@@ -9,19 +9,17 @@
 struct DescriptorType {
     static const char Bool = 'B';
     static const char Int = 'L';
-    static const char Int64 = 'L';
     static const char UInt = 'l';
-    static const char UInt64 = 'l';
-    static const char Int32 = 'K';
-    static const char UInt32 = 'k';
-    static const char Int16 = 'H';
-    static const char UInt16 = 'h';
     static const char Int8 = 'D';
     static const char UInt8 = 'd';
+    static const char Int16 = 'H';
+    static const char UInt16 = 'h';
+    static const char Int32 = 'K';
+    static const char UInt32 = 'k';
     static const char Int128 = 'X';
     static const char UInt128 = 'x';
     static const char Float = 'F';
-    static const char Double = 'D';
+    static const char Double = 'O';
     static const char Float128 = 'W';
     static const char String = 'S';
     static const char Void = 'V';
@@ -45,10 +43,39 @@ protected:
     std::string rawDescriptor;
 };
 
+#define DECLARE_DESCRIPTOR(name, type) \
+class name##Descriptor : public Descriptor { \
+public: \
+    using Ptr = std::shared_ptr<name##Descriptor>; \
+    name##Descriptor() { \
+        rawDescriptor = type; \
+    } \
+};
+
+DECLARE_DESCRIPTOR(Bool,        DescriptorType::Bool)
+DECLARE_DESCRIPTOR(Int,         DescriptorType::Int)
+DECLARE_DESCRIPTOR(UInt,        DescriptorType::UInt)
+DECLARE_DESCRIPTOR(Int8,        DescriptorType::Int8)
+DECLARE_DESCRIPTOR(UInt8,       DescriptorType::UInt8)
+DECLARE_DESCRIPTOR(Int16,       DescriptorType::Int16)
+DECLARE_DESCRIPTOR(UInt16,      DescriptorType::UInt16)
+DECLARE_DESCRIPTOR(Int32,       DescriptorType::Int32)
+DECLARE_DESCRIPTOR(UInt32,      DescriptorType::UInt32)
+DECLARE_DESCRIPTOR(Int128,      DescriptorType::Int128)
+DECLARE_DESCRIPTOR(UInt128,     DescriptorType::UInt128)
+DECLARE_DESCRIPTOR(Float,       DescriptorType::Float)
+DECLARE_DESCRIPTOR(Double,      DescriptorType::Double)
+DECLARE_DESCRIPTOR(Float128,    DescriptorType::Float128)
+DECLARE_DESCRIPTOR(String,      DescriptorType::String)
+DECLARE_DESCRIPTOR(Void,        DescriptorType::Void)
+
 // Represent an Variable
 class VariableDescriptor: public Descriptor {
 public:
-    VariableDescriptor(const std::string& filename);
+    VariableDescriptor(Descriptor::Ptr parent, const std::string& name);
+    
+    Descriptor::Ptr parentDescriptor;
+    Descriptor::Ptr typeDescriptor = nullptr;
 };
 
 class FunctionDescriptor: public Descriptor {
--- unittests/testcases/common_descriptor_test.cpp
+++ unittests/testcases/common_descriptor_test.cpp
@@ -12,3 +12,22 @@ TEST(DescriptorTest, FileModileDescritpor) {
     EXPECT_EQ(rawDescriptor2, "#bar/foo;");
     
 }
+
+TEST(DescriptorTest, PrimaryTypeChecker) {
+    EXPECT_EQ(BoolDescriptor().getRawDescriptor(), "B");
+    EXPECT_EQ(IntDescriptor().getRawDescriptor(), "L");
+    EXPECT_EQ(UIntDescriptor().getRawDescriptor(), "l");
+    EXPECT_EQ(Int8Descriptor().getRawDescriptor(), "D");
+    EXPECT_EQ(UInt8Descriptor().getRawDescriptor(), "d");
+    EXPECT_EQ(Int16Descriptor().getRawDescriptor(), "H");
+    EXPECT_EQ(UInt16Descriptor().getRawDescriptor(), "h");
+    EXPECT_EQ(Int32Descriptor().getRawDescriptor(), "K");
+    EXPECT_EQ(UInt32Descriptor().getRawDescriptor(), "k");
+    EXPECT_EQ(Int128Descriptor().getRawDescriptor(), "X");
+    EXPECT_EQ(UInt128Descriptor().getRawDescriptor(), "x");
+    EXPECT_EQ(FloatDescriptor().getRawDescriptor(), "F");
+    EXPECT_EQ(DoubleDescriptor().getRawDescriptor(), "O");
+    EXPECT_EQ(Float128Descriptor().getRawDescriptor(), "W");
+    EXPECT_EQ(StringDescriptor().getRawDescriptor(), "S");
+    EXPECT_EQ(VoidDescriptor().getRawDescriptor(), "V");
+}
